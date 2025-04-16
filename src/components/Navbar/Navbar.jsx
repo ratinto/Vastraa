a720d8c7b1a22cad84c552f2e8ce595a422e0206
Ritesh Kumar <7976ritesh@gmail.com>
2025-04-17 02:17:34 +0530

    sec

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { IoMdSearch } from "react-icons/io";
 import { FaCartShopping } from "react-icons/fa6";
 import { FaCaretDown } from "react-icons/fa";
-import DarkMode from "./DarkMode";
+// import DarkMode from "./DarkMode";
 import { FiShoppingBag } from "react-icons/fi";
 import { GiLoincloth } from "react-icons/gi";
 
@@ -91,7 +91,7 @@ const Navbar = ({ handleOrderPopup }) => {
 
             {/* Darkmode Switch */}
             <div>
-              <DarkMode />
+              {/* <DarkMode /> */}
             </div>
           </div>
         </div>
